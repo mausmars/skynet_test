15c97de3b94524259a7196904e59d7e5afa339dc
马里 <dalaoshuxxx@gmail.com>
2022-09-01 17:18:56 +0800

    云节点测试。。。

--- test_base/cluster_test/cluster1.lua
+++ test_base/cluster_test/cluster1.lua
@@ -3,24 +3,30 @@ local cluster = require "skynet.cluster"
 local snax = require "skynet.snax"
 
 skynet.start(function()
-	print("cluster1 start")
-	cluster.reload {
-		db = "127.0.0.1:2528",
-		db2 = "127.0.0.1:2529",
-	}
+    print("cluster1 start")
+    cluster.reload {
+        db1 = "127.0.0.1:2528",
+        db2 = "127.0.0.1:2529",
+    }
 
-	local sdb = skynet.newservice("simpledb")
-	-- register name "sdb" for simpledb, you can use cluster.query() later.
-	-- See cluster2.lua
-	cluster.register("sdb", sdb)
+    local sdb = skynet.newservice("simpledb")
+    -- register name "sdb" for simpledb, you can use cluster.query() later.
+    -- See cluster2.lua
+    cluster.register("sdb", sdb)        -- 将服务注册到云上 使用 @sdb
 
-	print(skynet.call(sdb, "lua", "SET", "a", "foobar"))
-	print(skynet.call(sdb, "lua", "SET", "b", "foobar2"))
-	print(skynet.call(sdb, "lua", "GET", "a"))
-	print(skynet.call(sdb, "lua", "GET", "b"))
 
-	cluster.open "db"
-	cluster.open "db2"
-	-- unique snax service
-	--snax.uniqueservice "pingserver"
+    print("set a= foobar")
+    skynet.call(sdb, "lua", "SET", "a", "foobar")
+    print("set b= foobar2")
+    skynet.call(sdb, "lua", "SET", "b", "foobar2")
+    print("get a= " .. skynet.call(sdb, "lua", "GET", "a"))
+    print("get b= " .. skynet.call(sdb, "lua", "GET", "b"))
+
+    -- node@servcie
+    -- db1@sdb
+    cluster.open("db1")     -- 开启节点
+    cluster.open("db2")     -- 开启节点
+
+    -- unique snax service
+    --snax.uniqueservice "pingserver"
 end)
--- test_base/cluster_test/cluster2.lua
+++ test_base/cluster_test/cluster2.lua
@@ -3,19 +3,26 @@ local cluster = require "skynet.cluster"
 
 skynet.start(function()
     print("cluster2 start")
-    local proxy = cluster.proxy("db", "@sdb")
-    --local proxy = cluster.proxy "db@sdb"
+    local proxy = cluster.proxy("db1", "@sdb")
+    print("create node proxy " .. proxy)
+
+    --local proxy = cluster.proxy "db1@sdb"
     local largekey = string.rep("X", 128 * 1024)
     local largevalue = string.rep("R", 100 * 1024)
+    -- skynet.call (云代理...
     skynet.call(proxy, "lua", "SET", largekey, largevalue)
 
     local v = skynet.call(proxy, "lua", "GET", largekey)
-    assert(largevalue == v)
+    if largevalue == v then
+        print("largevalue == v")
+    end
+
     --skynet.send(proxy, "lua", "PING", "proxy")
 
     skynet.fork(function()
         skynet.trace("cluster")
-        print(cluster.call("db", "@sdb", "GET", "a"))
+        -- cluster.call (node, @service...
+        print(cluster.call("db1", "@sdb", "GET", "a"))
         print(cluster.call("db2", "@sdb", "GET", "b"))
         cluster.send("db2", "@sdb", "PING", "db2:longstring" .. largevalue)
     end)
@@ -23,12 +30,16 @@ skynet.start(function()
     -- test snax service
     skynet.timeout(300, function()
         cluster.reload {
-            db = false, -- db is down
+            db1 = false, -- db1 is down
             db3 = "127.0.0.1:2529"
         }
-        print(pcall(cluster.call, "db", "@sdb", "GET", "a"))    -- db is down
+        print("111 ", pcall(cluster.call, "db1", "@sdb", "GET", "a"))    -- db is down
     end)
+
+    -- db3 不存在，阻塞等待，3秒后reload db3 映射到节点db2上，获取到数据
     cluster.reload { __nowaiting = false }
-    local pingserver = cluster.snax("db3", "pingserver")
-    print(pingserver.req.ping "hello")
+    print("222 ", pcall(cluster.call, "db3", "@sdb", "GET", "a"))
+
+    --local pingserver = cluster.snax("db3", "pingserver")
+    --print(pingserver.req.ping "hello")
 end)
